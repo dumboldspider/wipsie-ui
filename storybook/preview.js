4b50a68fc27f5f1fbc21780c48400d9abce19a68
danbrown <danielbrown25.9.2@gmail.com>
2021-10-12 06:44:57 -0300

    storybook preview

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,27 +1,27 @@
 import React from "react";
 import CssBaseline from "../src/components/CssBaseline/CssBaseline";
 
-// export const decorators = [
-//   (Story) => {
-//     return (
-//       <div
-//         style={{
-//           display: "flex",
-//           flex: 1,
-//           flexDirection: "column",
-//           justifyContent: "flex-start",
-//           alignItems: "center",
-//           flexWrap: "wrap",
-//           width: "100%",
-//           height: "100vh",
-//         }}
-//       >
-//         <CssBaseline />
-//         <Story />
-//       </div>
-//     );
-//   },
-// ];
+export const decorators = [
+  (Story) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          flexWrap: "wrap",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <CssBaseline />
+        <Story />
+      </div>
+    );
+  },
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
