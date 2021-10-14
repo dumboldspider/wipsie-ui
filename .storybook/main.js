2687c384ec6bb1377538eed28c6a20ebcde7a69a
danbrown <danielbrown25.9.2@gmail.com>
2021-10-14 14:57:31 -0300

    classes jsx fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -31,6 +31,11 @@ module.exports = {
       loader: require.resolve("babel-loader"),
       options: {
         presets: [["react-app", { flow: false, typescript: true }]],
+        plugins: [
+          "@babel/plugin-proposal-class-properties",
+          "@babel/plugin-proposal-private-methods",
+          "styled-jsx/babel",
+        ],
       },
     });
     config.resolve.extensions.push(".ts", ".tsx");
