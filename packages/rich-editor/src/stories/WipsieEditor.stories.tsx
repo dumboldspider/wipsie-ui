fb1758ac9ef313caf19eee0f6263170e7f428a3b
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-04-14 20:30:36 -0300

    Rich editor stories

--- packages/rich-editor/src/stories/WipsieEditor.stories.tsx
+++ packages/rich-editor/src/stories/WipsieEditor.stories.tsx
@@ -38,6 +38,7 @@ import {
   defaultToolbar,
   basicToolbar,
   fullToolbar,
+  ToolBarType,
 } from "../index";
 import "../components/wipsie-editor.css";
 
@@ -49,7 +50,128 @@ export default {
   },
 };
 
-export const HTMLEditorWithToolbar = () => {
+export const HTMLEditorWithBasicToolbar = () => {
+  const initialValue = `
+  <h1 align="center">Hello World</h1>
+  <p>This is a simple <span class="wuire-rgb">example</span><b></b> of a React component.</p><p><br></p>
+  <p>Click on the box to: <span class="wuire-spoiler" onclick="this.classList.toggle('wuire-spoiler-open');">See the Spoiler</span></p>
+`;
+
+  const [value, setValue] = useState(initialValue);
+  const [theme, setTheme] = useState<"light" | "dark" | "cosmic">("light");
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  const handleThemeChange = () => {
+    setTheme((prev) => {
+      if (prev === "light") {
+        return "dark";
+      } else if (prev === "dark") {
+        return "cosmic";
+      } else {
+        return "light";
+      }
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Page
+        className="App"
+        backgroundColor="shade"
+        display="flex"
+        direction="column"
+        align="center"
+        justify="center"
+        p={2}
+      >
+        <Button backgroundColor="primary" onClick={handleThemeChange}>
+          {theme}
+        </Button>
+
+        <Flex fullWidth maxWidth="700px">
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Editor
+            <Typography variant="body1" color="text">
+              (What you See is What you Get)
+            </Typography>
+          </Typography>
+          <Spacing height={1} />
+
+          <Typography variant="h4" color="text">
+            Basic Toolbar
+          </Typography>
+          <WipsieHTMLEditor
+            id="editor-test-2"
+            initialValue={initialValue}
+            value={value}
+            onChange={onChange}
+            debug={false}
+            toolbar={basicToolbar}
+          />
+
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output HTML
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <code>{value}</code>
+          </Container>
+
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output Render
+            <Typography variant="body1" color="text">
+              (intended to be the same as you see in the editor)
+            </Typography>
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <HTMLOutputContainer dangerouslySetInnerHTML={{ __html: value }} />
+          </Container>
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Commands
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            Ctrl + B = Bold
+            <br />
+            Ctrl + I = Italic
+            <br />
+            Ctrl + U = Underline
+            <br />
+            Ctrl + Z = Undo
+            <br />
+            Ctrl + Y = Redo
+            <br />
+            Ctrl + A = Select All
+            <br />
+            Ctrl + C = Copy
+            <br />
+            Ctrl + V = Paste
+            <br />
+            Ctrl + X = Cut
+            <br />
+            Esc = Clear Format
+          </Container>
+
+          <Spacing height={5} />
+        </Flex>
+      </Page>
+    </ThemeProvider>
+  );
+};
+
+export const HTMLEditorWithDefaultToolbar = () => {
   const initialValue = `
   <h1 align="center">Hello World</h1>
   <p>This is a simple <span class="wuire-rgb">example</span><b></b> of a React component.</p><p><br></p>
@@ -105,7 +227,7 @@ export const HTMLEditorWithToolbar = () => {
             Default Toolbar
           </Typography>
           <WipsieHTMLEditor
-            id="editor-test"
+            id="editor-test-2"
             initialValue={initialValue}
             value={value}
             onChange={onChange}
@@ -113,6 +235,113 @@ export const HTMLEditorWithToolbar = () => {
             toolbar={defaultToolbar}
           />
 
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output HTML
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <code>{value}</code>
+          </Container>
+
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output Render
+            <Typography variant="body1" color="text">
+              (intended to be the same as you see in the editor)
+            </Typography>
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <HTMLOutputContainer dangerouslySetInnerHTML={{ __html: value }} />
+          </Container>
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Commands
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            Ctrl + B = Bold
+            <br />
+            Ctrl + I = Italic
+            <br />
+            Ctrl + U = Underline
+            <br />
+            Ctrl + Z = Undo
+            <br />
+            Ctrl + Y = Redo
+            <br />
+            Ctrl + A = Select All
+            <br />
+            Ctrl + C = Copy
+            <br />
+            Ctrl + V = Paste
+            <br />
+            Ctrl + X = Cut
+            <br />
+            Esc = Clear Format
+          </Container>
+
+          <Spacing height={5} />
+        </Flex>
+      </Page>
+    </ThemeProvider>
+  );
+};
+
+export const HTMLEditorWithFullToolbar = () => {
+  const initialValue = `
+  <h1 align="center">Hello World</h1>
+  <p>This is a simple <span class="wuire-rgb">example</span><b></b> of a React component.</p><p><br></p>
+  <p>Click on the box to: <span class="wuire-spoiler" onclick="this.classList.toggle('wuire-spoiler-open');">See the Spoiler</span></p>
+`;
+
+  const [value, setValue] = useState(initialValue);
+  const [theme, setTheme] = useState<"light" | "dark" | "cosmic">("light");
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  const handleThemeChange = () => {
+    setTheme((prev) => {
+      if (prev === "light") {
+        return "dark";
+      } else if (prev === "dark") {
+        return "cosmic";
+      } else {
+        return "light";
+      }
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Page
+        className="App"
+        backgroundColor="shade"
+        display="flex"
+        direction="column"
+        align="center"
+        justify="center"
+        p={2}
+      >
+        <Button backgroundColor="primary" onClick={handleThemeChange}>
+          {theme}
+        </Button>
+
+        <Flex fullWidth maxWidth="700px">
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Editor
+            <Typography variant="body1" color="text">
+              (What you See is What you Get)
+            </Typography>
+          </Typography>
           <Spacing height={1} />
 
           <Typography variant="h4" color="text">
@@ -127,10 +356,122 @@ export const HTMLEditorWithToolbar = () => {
             toolbar={fullToolbar}
           />
 
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output HTML
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <code>{value}</code>
+          </Container>
+
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Output Render
+            <Typography variant="body1" color="text">
+              (intended to be the same as you see in the editor)
+            </Typography>
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            <HTMLOutputContainer dangerouslySetInnerHTML={{ __html: value }} />
+          </Container>
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Commands
+          </Typography>
+          <Spacing height={1} />
+          <Container fullWidth>
+            Ctrl + B = Bold
+            <br />
+            Ctrl + I = Italic
+            <br />
+            Ctrl + U = Underline
+            <br />
+            Ctrl + Z = Undo
+            <br />
+            Ctrl + Y = Redo
+            <br />
+            Ctrl + A = Select All
+            <br />
+            Ctrl + C = Copy
+            <br />
+            Ctrl + V = Paste
+            <br />
+            Ctrl + X = Cut
+            <br />
+            Esc = Clear Format
+          </Container>
+
+          <Spacing height={5} />
+        </Flex>
+      </Page>
+    </ThemeProvider>
+  );
+};
+
+export const HTMLEditorWithCustomToolbar = () => {
+  const initialValue = `
+  <h1 align="center">Hello World</h1>
+  <p>This is a simple <span class="wuire-rgb">example</span><b></b> of a React component.</p><p><br></p>
+  <p>Click on the box to: <span class="wuire-spoiler" onclick="this.classList.toggle('wuire-spoiler-open');">See the Spoiler</span></p>
+`;
+
+  const [value, setValue] = useState(initialValue);
+  const [theme, setTheme] = useState<"light" | "dark" | "cosmic">("light");
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  const handleThemeChange = () => {
+    setTheme((prev) => {
+      if (prev === "light") {
+        return "dark";
+      } else if (prev === "dark") {
+        return "cosmic";
+      } else {
+        return "light";
+      }
+    });
+  };
+
+  const toolbar: ToolBarType = [
+    [imageTool, boldTool, emojiTool],
+    [italicTool, linkTool],
+  ];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Page
+        className="App"
+        backgroundColor="shade"
+        display="flex"
+        direction="column"
+        align="center"
+        justify="center"
+        p={2}
+      >
+        <Button backgroundColor="primary" onClick={handleThemeChange}>
+          {theme}
+        </Button>
+
+        <Flex fullWidth maxWidth="700px">
+          <Spacing height={5} />
+
+          <Typography variant="h2" color="text">
+            Editor
+            <Typography variant="body1" color="text">
+              (What you See is What you Get)
+            </Typography>
+          </Typography>
           <Spacing height={1} />
 
           <Typography variant="h4" color="text">
-            Basic Toolbar
+            Custom Toolbar
           </Typography>
           <WipsieHTMLEditor
             id="editor-test-2"
@@ -138,7 +479,7 @@ export const HTMLEditorWithToolbar = () => {
             value={value}
             onChange={onChange}
             debug={false}
-            toolbar={basicToolbar}
+            toolbar={toolbar}
           />
 
           <Spacing height={5} />
