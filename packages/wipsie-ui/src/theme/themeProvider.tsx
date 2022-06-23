869fe44ad725d45af27522aa8b3c719c4db6fc6b
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-06-23 19:23:39 -0300

    theme provider lighter

--- packages/wipsie-ui/src/theme/themeProvider.tsx
+++ packages/wipsie-ui/src/theme/themeProvider.tsx
@@ -6,55 +6,51 @@ import lightTheme from "./variants/light";
 import darkTheme from "./variants/dark";
 import cosmicTheme from "./variants/cosmic";
 import { ThemeParam, getThemeByType, mergeTheme, switchTheme } from "./utils";
+import { CssBaseline } from "../components/CssBaseline";
 
 export interface Props {
   theme?: ThemeParam | string;
+  baseline?: boolean;
 }
 
 const ThemeProvider: React.FC<PropsWithChildren<Props>> = ({
   children,
   theme,
+  baseline = true,
 }) => {
-  if (typeof theme === "string") {
-    switch (theme) {
-      case "dark":
-        return (
-          <ThemeContext.Provider value={darkTheme}>
-            {children}
-          </ThemeContext.Provider>
-        );
-      case "cosmic":
-        return (
-          <ThemeContext.Provider value={cosmicTheme}>
-            {children}
-          </ThemeContext.Provider>
-        );
-      case "light":
-      default:
-        return (
-          <ThemeContext.Provider value={lightTheme}>
-            {children}
-          </ThemeContext.Provider>
-        );
-    }
-  } else {
-    //  Case of theme being different from defaults
+  //  Case of theme being different from defaults
+  let userTheme = lightTheme; // default theme
 
+  if (typeof theme !== "string") {
     const customTheme = theme as WipsieThemes;
     const currentTheme = useTheme();
     const baseTheme = customTheme.type
       ? getThemeByType(customTheme.type)
       : currentTheme;
     const merged = mergeTheme(baseTheme, customTheme);
-    const userTheme =
-      currentTheme.type !== merged.type ? switchTheme(merged) : merged;
 
-    return (
-      <ThemeContext.Provider value={userTheme}>
-        {children}
-      </ThemeContext.Provider>
-    );
+    // Case of theme being different from defaults
+    userTheme =
+      currentTheme.type !== merged.type ? switchTheme(merged) : merged;
   }
+  return (
+    <ThemeContext.Provider
+      value={
+        typeof theme === "string"
+          ? theme === "light"
+            ? lightTheme
+            : theme === "dark"
+            ? darkTheme
+            : theme === "cosmic"
+            ? cosmicTheme
+            : userTheme // default
+          : userTheme
+      }
+    >
+      {baseline && <CssBaseline />}
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
