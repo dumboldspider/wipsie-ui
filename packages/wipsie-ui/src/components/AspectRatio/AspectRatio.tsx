7ec6b14a6413b7a8e04f43b8c2b9b7eb9c14ac92
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-08-17 20:06:26 -0300

    fixed aspect ratio ref

--- packages/wipsie-ui/src/components/AspectRatio/AspectRatio.tsx
+++ packages/wipsie-ui/src/components/AspectRatio/AspectRatio.tsx
@@ -1,57 +1,63 @@
 // Generated with util/create-component.js
-import React, { useMemo } from "react";
+import React, { ForwardedRef, forwardRef, useMemo } from "react";
 import classnames from "classnames";
 import { AspectRatioProps } from "./AspectRatio.types";
 
-export const AspectRatio: React.FC<AspectRatioProps> = (props) => {
-  const {
-    aspectRatio = "100%",
-    internalProps = {},
-    children,
-    className,
-    ...otherProps
-  } = props;
+export const AspectRatio = forwardRef(
+  (props: AspectRatioProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const {
+      aspectRatio = "100%",
+      internalProps = {},
+      children,
+      className,
+      ...otherProps
+    } = props;
 
-  const handleAspectRatio = useMemo(() => {
-    if (aspectRatio.includes("%")) {
-      return aspectRatio;
-    } else {
-      const ratio = aspectRatio.split(":");
-      return `${
-        100 /
-        parseFloat((parseFloat(ratio[0]) / parseFloat(ratio[1])).toFixed(2))
-      }%`;
-    }
-  }, [aspectRatio]);
+    const handleAspectRatio = useMemo(() => {
+      if (aspectRatio.includes("%")) {
+        return aspectRatio;
+      } else {
+        const ratio = aspectRatio.split(":");
+        return `${
+          100 /
+          parseFloat((parseFloat(ratio[0]) / parseFloat(ratio[1])).toFixed(2))
+        }%`;
+      }
+    }, [aspectRatio]);
 
-  return (
-    <div className={classnames("Wps-AspectRatio", className)} {...otherProps}>
+    return (
       <div
-        className={classnames("Wps-AspectRatioInternal", className)}
-        {...internalProps}
+        ref={ref}
+        className={classnames("Wps-AspectRatio", className)}
+        {...otherProps}
       >
-        {children}
-      </div>
+        <div
+          className={classnames("Wps-AspectRatioInternal", className)}
+          {...internalProps}
+        >
+          {children}
+        </div>
 
-      <style jsx>{`
-        .Wps-AspectRatio {
-          width: 100%;
-          background: transparent;
-          position: relative;
-          padding-bottom: ${handleAspectRatio};
-        }
-        .Wps-AspectRatio .Wps-AspectRatioInternal {
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background: transparent;
-          overflow: hidden;
-          width: 100%;
-          height: 100%;
-        }
-      `}</style>
-    </div>
-  );
-};
+        <style jsx>{`
+          .Wps-AspectRatio {
+            width: 100%;
+            background: transparent;
+            position: relative;
+            padding-bottom: ${handleAspectRatio};
+          }
+          .Wps-AspectRatio .Wps-AspectRatioInternal {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: transparent;
+            overflow: hidden;
+            width: 100%;
+            height: 100%;
+          }
+        `}</style>
+      </div>
+    );
+  }
+);
