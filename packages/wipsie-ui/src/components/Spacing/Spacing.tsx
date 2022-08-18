0527ba8431355db286c5a6da2b88f96d8b3daeb0
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-08-18 18:03:57 -0300

    added spacing ref

--- packages/wipsie-ui/src/components/Spacing/Spacing.tsx
+++ packages/wipsie-ui/src/components/Spacing/Spacing.tsx
@@ -1,68 +1,79 @@
 // Generated with util/create-component.js
-import React from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import classnames from "classnames";
 import useTheme from "../../hooks/useTheme";
 import { SpacingProps } from "./Spacing.types";
 import { getBreakpoints } from "../Flex/Flex.functions";
 
-export const Spacing: React.FC<SpacingProps> = (props) => {
-  const theme = useTheme();
-  const { height, width, className = null, ...otherProps } = props;
+export const Spacing = forwardRef(
+  (props: SpacingProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const theme = useTheme();
+    const { height, width, className = null, ...otherProps } = props;
 
-  function handleValue(value) {
-    return value
-      ? typeof value === "number"
-        ? `${value * theme.layout.spacingUnit}px`
-        : value
-      : "0px";
+    function handleValue(value) {
+      return value
+        ? typeof value === "number"
+          ? `${value * theme.layout.spacingUnit}px`
+          : value
+        : "0px";
+    }
+
+    return (
+      <div
+        data-testid="Wps-Spacing"
+        className={classnames("Wps-Spacing", className)}
+        ref={ref}
+        {...otherProps}
+      >
+        <style jsx>{`
+          .Wps-Spacing {
+            width: ${width ? getBreakpoints(width, "xs", handleValue) : "100%"};
+            height: ${height
+              ? getBreakpoints(height, "xs", handleValue)
+              : "100%"};
+          }
+          @media only screen and (min-width: ${theme.breakpoints.sm.min}) {
+            .Wps-Spacing {
+              width: ${width
+                ? getBreakpoints(width, "sm", handleValue)
+                : "100%"};
+              height: ${height
+                ? getBreakpoints(height, "sm", handleValue)
+                : "100%"};
+            }
+          }
+          @media only screen and (min-width: ${theme.breakpoints.md.min}) {
+            .Wps-Spacing {
+              width: ${width
+                ? getBreakpoints(width, "md", handleValue)
+                : "100%"};
+              height: ${height
+                ? getBreakpoints(height, "md", handleValue)
+                : "100%"};
+            }
+          }
+          @media only screen and (min-width: ${theme.breakpoints.lg.min}) {
+            .Wps-Spacing {
+              width: ${width
+                ? getBreakpoints(width, "lg", handleValue)
+                : "100%"};
+              height: ${height
+                ? getBreakpoints(height, "lg", handleValue)
+                : "100%"};
+            }
+          }
+          @media only screen and (min-width: ${theme.breakpoints.xl.min}) {
+            .Wps-Spacing {
+              width: ${width
+                ? getBreakpoints(width, "xl", handleValue)
+                : "100%"};
+              height: ${height
+                ? getBreakpoints(height, "xl", handleValue)
+                : "100%"};
+            }
+          }
+        `}</style>
+      </div>
+    );
   }
-
-  return (
-    <div
-      data-testid="Wps-Spacing"
-      className={classnames("Wps-Spacing", className)}
-      {...otherProps}
-    >
-      <style jsx>{`
-        .Wps-Spacing {
-          width: ${width ? getBreakpoints(width, "xs", handleValue) : "100%"};
-          height: ${height
-            ? getBreakpoints(height, "xs", handleValue)
-            : "100%"};
-        }
-        @media only screen and (min-width: ${theme.breakpoints.sm.min}) {
-          .Wps-Spacing {
-            width: ${width ? getBreakpoints(width, "sm", handleValue) : "100%"};
-            height: ${height
-              ? getBreakpoints(height, "sm", handleValue)
-              : "100%"};
-          }
-        }
-        @media only screen and (min-width: ${theme.breakpoints.md.min}) {
-          .Wps-Spacing {
-            width: ${width ? getBreakpoints(width, "md", handleValue) : "100%"};
-            height: ${height
-              ? getBreakpoints(height, "md", handleValue)
-              : "100%"};
-          }
-        }
-        @media only screen and (min-width: ${theme.breakpoints.lg.min}) {
-          .Wps-Spacing {
-            width: ${width ? getBreakpoints(width, "lg", handleValue) : "100%"};
-            height: ${height
-              ? getBreakpoints(height, "lg", handleValue)
-              : "100%"};
-          }
-        }
-        @media only screen and (min-width: ${theme.breakpoints.xl.min}) {
-          .Wps-Spacing {
-            width: ${width ? getBreakpoints(width, "xl", handleValue) : "100%"};
-            height: ${height
-              ? getBreakpoints(height, "xl", handleValue)
-              : "100%"};
-          }
-        }
-      `}</style>
-    </div>
-  );
-};
+);
